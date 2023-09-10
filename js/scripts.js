b70a78e4c3561319f97e09045d14f7999c334069
Rômulo Junior <132522416+parrylt@users.noreply.github.com>
2023-09-10 11:26:50 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+﻿
+
+//funções
+
+
+function bomdia () {
+	alert ("Boa noite")
+}
+
+function pintatela () {
+	document.body.style.backgroundColor = "black";
+	document.body.style.font
+}
+
+function despintatela () {
+	document.body.style.backgroundColor = "white";
+}
+
+
+
+var nome=prompt ("Digite o seu nome");
+
+
+function repetenome (){
+var x= 0
+	while(x <=20)
+{
+	document.write(x + " " + nome + "<br>");
+	x++
+}
+
+
+}
